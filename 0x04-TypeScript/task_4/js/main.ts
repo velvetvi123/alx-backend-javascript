46a307b1531176ba63bb1a382401a0784a3485ec
velvetvi123 <vic_velvet@yahoo.com>
2024-10-10 11:31:23 -0700

    done

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,11 @@
+namespace Subjects {
+    export const cpp = Cpp;
+    export const java = Java;
+    export const react = React;
+    // @ts-ignore
+    export const cTeacher: Teacher
+    {
+        "test";
+        "test";
+        10
+    }
